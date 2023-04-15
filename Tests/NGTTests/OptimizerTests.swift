6d591b89e06f3e34a2b1157d639e97793db78026
Francis Chong <francis@goodnotesapp.com>
2023-04-15 19:17:58 +0100

    fix tests

--- Tests/NGTTests/OptimizerTests.swift
+++ Tests/NGTTests/OptimizerTests.swift
@@ -2,20 +2,20 @@ import XCTest
 @testable import NGT
 
 final class OptimizerTests: XCTestCase {
-    var testFolder: String!
+    var testFolder: URL!
 
     override func setUpWithError() throws {
-        testFolder = FileManager.default.temporaryDirectory.path.appending("OptimizerTests")
-        try FileManager.default.createDirectory(atPath: testFolder, withIntermediateDirectories: true)
+        testFolder = FileManager.default.temporaryDirectory.appendingPathComponent("OptimizerTests")
+        try FileManager.default.createDirectory(atPath: testFolder.path, withIntermediateDirectories: true)
     }
 
     override func tearDownWithError() throws {
         guard let testFolder = testFolder else { return }
-        _ = try? FileManager.default.removeItem(atPath: testFolder)
+        _ = try? FileManager.default.removeItem(atPath: testFolder.path)
     }
 
     func testOptimize() throws {
-        let indexPath = testFolder.appending("index")
+        let indexPath = testFolder.appendingPathComponent("index").path
         let objects: [[Double]] = [
             [1, 1, 2, 1],
             [5, 4, 6, 5],
@@ -27,7 +27,7 @@ final class OptimizerTests: XCTestCase {
         XCTAssertTrue(FileManager.default.fileExists(atPath: indexPath))
 
         // create temporary folder
-        let optimizerPath = testFolder.appending("optimized")
+        let optimizerPath = testFolder.appendingPathComponent("optimized").path
         let optimizer = Optimizer(queries: 1)
         try optimizer.adjustSearchCoefficients(index: index)
         try optimizer.execute(inIndex: index, outIndexPath: optimizerPath)
