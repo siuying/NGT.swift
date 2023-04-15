2743eb6b22283a9e3141262ee0141236ddd283d0
Francis Chong <francis@goodnotesapp.com>
2023-04-15 18:56:48 +0100

    lower target os

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "NGT",
     platforms: [
         .macOS(.v13),
-        .iOS(.v13)
+        .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
